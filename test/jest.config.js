3c27e23e4029059aab55ff5605905ca1ab33586a
iamcalegari <iamcalegari20@gmail.com>
2024-05-13 10:38:10 -0300

    fix: jest.config

--- test/jest.config.js
+++ test/jest.config.js
@@ -1,13 +1,12 @@
-const { resolve } = require("path");
+const { resolve } = require('path');
 
-const { rootDir, testMatch } = require("../jest.config");
-const root = resolve(__dirname, "..");
+const root = resolve(__dirname, '..');
 const rootConfig = require(`${root}/jest.config.js`);
 
 module.exports = {
   ...rootConfig,
   rootDir: root,
-  displayName: "end2end-tests",
-  setupFilesAfterEnv: ["<rootDir>/test/jest-setup.ts"], // Importante
-  testMatch: ["<rootDir>/test/**/*.test.ts"],
+  displayName: 'end2end-tests',
+  setupFilesAfterEnv: ['<rootDir>/test/jest-setup.ts'], // Importante
+  testMatch: ['<rootDir>/test/**/*.test.ts'],
 };
